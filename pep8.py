a5922b609bc1363b230e67789993819d5d994564
MartinezAguilera <martinezj85@uabc.edu.mx>
2023-05-02 19:40:12 -0700

    Commit pep8

---
+++ pep8.py
@@ -0,0 +1,10 @@
+class User():
+
+    def __init__(self,username, password=''):
+    self.username = username
+    self.password = password
+
+    def set_password(self):
+        pass
+
+    codyuser = user('Cody')
